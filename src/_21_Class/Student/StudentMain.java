fc49ded0e9d67455d523b85c3b529a5b246663a6
gyu-min3610 <mrkwon3610@naver.com>
2025-06-07 00:03:20 +0900

    Feat: student

--- src/_21_Class/Student/StudentMain.java
+++ src/_21_Class/Student/StudentMain.java
@@ -2,13 +2,15 @@ package _21_Class.Student;
 
 public class StudentMain {
     public static void main(String[] args) {
-        Student student = new Student();
-
-        student.name = "규민";
-        student.age = 29;
-        student.email = "zpstls2217@naver.com";
-        student.address = "부산";
-
+        Student student = new Student("규민",29,"zpstls2217@naver.com","부산");
+            student.setName("몽돌");
+            student.setAge(3);
+//        student.name = "규민";
+//        student.age = 29;
+//        student.email = "zpstls2217@naver.com";
+//        student.address = "부산";
+//        System.out.println(student.name);
+        System.out.println(student.getEmail());
         student.showInfo();
     }
 }
